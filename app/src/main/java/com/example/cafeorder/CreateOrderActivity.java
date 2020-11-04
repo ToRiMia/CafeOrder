6bf03f415f18e6cd738d02078ee99a59a8acefa5
Viktoriia Omelchenko <omelchenko.viktoriia7@gmail.com>
2020-11-04 18:36:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/cafeorder/CreateOrderActivity.java
@@ -0,0 +1,101 @@
+package com.example.cafeorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class CreateOrderActivity extends AppCompatActivity {
+
+    private TextView textViewHello;
+    private TextView textViewAdditions;
+    private CheckBox checkBoxMilk;
+    private CheckBox checkBoxSugar;
+    private CheckBox checkBoxLemon;
+    private Spinner spinnerTea;
+    private Spinner spinnerCoffee;
+
+    private String drink;
+    private String password;
+    private String name;
+    private StringBuilder builderAdditions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_order);
+        Intent intent = getIntent();
+        if (intent.hasExtra("name") && intent.hasExtra("password")) {
+            name = intent.getStringExtra("name");
+            password = intent.getStringExtra("password");
+        } else {
+            name = getString(R.string.default_name);
+            password = getString(R.string.default_password);
+        }
+        drink = getString(R.string.tea);
+        textViewHello = findViewById(R.id.textViewHello);
+        String hello = String.format(getString(R.string.hello_user), name);
+        textViewHello.setText(hello);
+        textViewAdditions = findViewById(R.id.textViewAddition);
+        String additions = getString(R.string.additions_tea);
+        textViewAdditions.setText(additions);
+        checkBoxMilk = findViewById(R.id.checkBoxMilk);
+        checkBoxSugar = findViewById(R.id.checkBoxSugar);
+        checkBoxLemon = findViewById(R.id.checkBoxLemon);
+        spinnerTea = findViewById(R.id.spinnerTea);
+        spinnerCoffee = findViewById(R.id.spinnerCoffee);
+        builderAdditions = new StringBuilder();
+    }
+
+    public void onClickChangeDrink(View view) {
+        RadioButton button = (RadioButton) view;
+        int id = button.getId();
+        String additions;
+        if (id == R.id.radioButtonTea) {
+            drink = getString(R.string.tea);
+            additions = getString(R.string.additions_tea);
+            spinnerTea.setVisibility(View.VISIBLE);
+            spinnerCoffee.setVisibility(View.INVISIBLE);
+            checkBoxLemon.setVisibility(View.VISIBLE);
+        } else {
+            drink = getString(R.string.coffee);
+            additions = getString(R.string.additions_coffee);
+            spinnerCoffee.setVisibility(View.VISIBLE);
+            spinnerTea.setVisibility(View.INVISIBLE);
+            checkBoxLemon.setVisibility(View.INVISIBLE);
+        }
+        textViewAdditions.setText(additions);
+    }
+
+    public void onClickSendOrder(View view) {
+        builderAdditions.setLength(0);
+        if (checkBoxMilk.isChecked()) {
+            builderAdditions.append(getString(R.string.milk)).append(" ");
+        }
+        if (checkBoxSugar.isChecked()) {
+            builderAdditions.append(getString(R.string.sugar)).append(" ");
+        }
+        if (checkBoxLemon.isChecked() && drink.equals(getString(R.string.tea))) {
+            builderAdditions.append(getString(R.string.lemon)).append(" ");
+        }
+        String optionOfDrink = "";
+        if (drink.equals(getString(R.string.tea))) {
+            optionOfDrink = spinnerTea.getSelectedItem().toString();
+        } else
+            optionOfDrink = spinnerCoffee.getSelectedItem().toString();
+        String order = String.format(getString(R.string.order), name, password, drink, optionOfDrink);
+        String additions;
+        if (builderAdditions.length() > 0) {
+            additions = getString(R.string.additions) + builderAdditions.toString();
+        } else additions = "";
+        String fullOrder = order + additions;
+        Intent intent = new Intent(this, OrderDetailActivity.class);
+        intent.putExtra("order", fullOrder);
+        startActivity(intent);
+    }
+}
